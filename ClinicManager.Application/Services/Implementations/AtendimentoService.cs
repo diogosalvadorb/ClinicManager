ae370b6d309290604633dc893d8b5b8912b28c9f
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-03-14 21:33:17 -0300

    implementação Service Atendimento

--- ClinicManager.Application/Services/Implementations/AtendimentoService.cs
+++ ClinicManager.Application/Services/Implementations/AtendimentoService.cs
@@ -1,33 +1,88 @@
 ﻿using ClinicManager.Application.Services.Interfaces;
 using ClinicManager.Core.Entities;
+using ClinicManager.Core.Repositories;
 
 namespace ClinicManager.Application.Services.Implementations
 {
     public class AtendimentoService : IAtendimentoService
     {
-        public Task<Atendimento> AddAsync(Atendimento atendimento)
+        private readonly IAtendimentoRepository _atendimentoRepository;
+        public AtendimentoService(IAtendimentoRepository atendimentoRepository)
         {
-            throw new NotImplementedException();
+            _atendimentoRepository = atendimentoRepository;
         }
 
         public Task<IEnumerable<Atendimento>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var atendimentos = _atendimentoRepository.GetAll();
+                if (atendimentos == null) return null; 
+                
+                return atendimentos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public Task<Atendimento> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var atendimento = _atendimentoRepository.GetById(id);
+                if (atendimento == null) return null;
+
+                return atendimento;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        public Task RemoverAsync(Guid id)
+        public Task<Atendimento> AddAsync(Atendimento atendimento)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var adicionarAtendimento = _atendimentoRepository.AddAsync(atendimento);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        public Task UpdateAsync(Atendimento atendimento)
+        public async Task UpdateAsync(Atendimento atendimento)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var buscarAtendimento = await _atendimentoRepository.GetById(atendimento.Id);
+                if (buscarAtendimento == null) throw new Exception("Atendimento para atualizar não encontrado.");
+
+                await _atendimentoRepository.UpdateAsync(atendimento);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
+
+        public async Task RemoverAsync(Guid id)
+        {
+            try
+            {
+                var buscaAtendimento = _atendimentoRepository.GetById(id);
+                if (buscaAtendimento == null) throw new Exception("Atendimento para remover não encontrado.");
+
+                await _atendimentoRepository.RemoverAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        } 
     }
 }
