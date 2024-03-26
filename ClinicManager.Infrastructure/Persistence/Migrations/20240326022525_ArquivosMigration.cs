43bac6e9bcdb3da0eb91a6918f842e3f4ec7df65
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-03-25 23:27:04 -0300

    entidade arquivo e migrations

---
+++ ClinicManager.Infrastructure/Persistence/Migrations/20240326022525_ArquivosMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicManager.Infrastructure.Persistence.Migrations
+{
+    public partial class ArquivosMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arquivos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    NomeArquivo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoConteudo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arquivos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Arquivos");
+        }
+    }
+}
