c88167bbf62f978f5b08b33d6e36a31d0b85ef77
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-04-04 18:48:15 -0300

    config jwt, entitie user

---
+++ ClinicManager.Application/Services/Implementations/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using ClinicManager.Application.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ClinicManager.Application.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(string login, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim("userName", login),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials,
+                claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        }
+    }
+}
---
+++ ClinicManager.Core/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicManager.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool Active { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public Paciente? Paciente { get; set; }
+        public Medico? Medico { get; set; }
+    }
+}
